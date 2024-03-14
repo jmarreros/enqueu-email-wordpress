8ddcb68f102082edcd5e4cbbe0c083733129e7da
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2024-03-14 08:18:05 -0500

    Agregado consistencia correo dominios temporales y existentes para no ser enviados

--- includes/process.php
+++ includes/process.php
@@ -2,73 +2,87 @@
 
 namespace dcms\enqueu\includes;
 
-use dcms\enqueu\includes\Database;
 use dcms\enqueu\helpers\State;
 
-class Process{
-    private $quantity_batch;
-    private $email_from;
-    private $email_from_name;
 
-    public function __construct(){
-        add_action('admin_post_process_force_sent', [$this, 'process_force_sent']);
-    }
+class Process {
+	private $email_from;
+	private $email_from_name;
 
-    // Force send mails queue
-    public function process_force_sent(){
-        $this->process_sent();
-        wp_redirect( admin_url( DCMS_ENQUEU_SUBMENU . '&page=enqueu-email' ) );
-    }
+	public function __construct() {
+		add_action( 'admin_post_process_force_sent', [ $this, 'process_force_sent' ] );
+	}
+
+	// Force send mails queue
+	public function process_force_sent() {
+		$this->process_sent();
+		wp_redirect( admin_url( DCMS_ENQUEU_SUBMENU . '&page=enqueu-email' ) );
+	}
 
 
-    // Filter from and name if exixts
-    private function filter_from_sender_mail(){
-        if ( $this->email_from ){
-            add_filter( 'wp_mail_from', function(){
-                return $this->email_from;
-            });
-        }
+	// Filter from and name if exixts
+	private function filter_from_sender_mail() {
+		if ( $this->email_from ) {
+			add_filter( 'wp_mail_from', function () {
+				return $this->email_from;
+			} );
+		}
 
-        if ( $this->email_from_name ){
-            add_filter( 'wp_mail_from_name', function(){
-                return $this->email_from_name;
-            });
-        }
-    }
+		if ( $this->email_from_name ) {
+			add_filter( 'wp_mail_from_name', function () {
+				return $this->email_from_name;
+			} );
+		}
+	}
 
-    public function process_sent(){
+	public function process_sent() {
+		$restrict_mails = [ '@emailtemp.com', '@emailexists.com' ];
 
-        $options = get_option(DCMS_ENQUEU_OPTIONS);
-        $this->quantity_batch = $options['dcms_quantity_batch'];
-        $this->email_from = $options['dcms_enqueue_from'];
-        $this->email_from_name = $options['dcms_enqueue_from_name'];
+		$options          = get_option( DCMS_ENQUEU_OPTIONS );
+		$quantity_batch   = $options['dcms_quantity_batch'];
+		$this->email_from = $options['dcms_enqueue_from'];
+		$this->email_from_name = $options['dcms_enqueue_from_name'];
 
-        $db = new Database();
+		$db = new Database();
 
-        $this->filter_from_sender_mail();
+		$this->filter_from_sender_mail();
 
-        $items = $db->get_pending_emails($this->quantity_batch);
-        
-        if ( $items ){
-            foreach( $items as $item){
-                $atts = json_decode(base64_decode($item->data), true);
+		$items = $db->get_pending_emails( $quantity_batch );
 
-                $to = $atts['to'];
-				$subject = $atts['subject'];
-				$message = $atts['message'];
-				$headers = $atts['headers'];
+		if ( $items ) {
+			foreach ( $items as $item ) {
+				$atts = json_decode( base64_decode( $item->data ), true );
+
+				$to          = $atts['to'];
+				$subject     = $atts['subject'];
+				$message     = $atts['message'];
+				$headers     = $atts['headers'];
 				$attachments = $atts['attachments'];
 
-                global $dcms_mail_real;
-                $dcms_mail_real = true; // real email sending
+				// Exclude restricted emails
+				$status = State::pending;
+				foreach ( $restrict_mails as $restrict_mail ) {
+					if ( strpos( $to, $restrict_mail ) !== false ) {
+						$status = State::error;
+						$db->update_email_status( $item->id, $status );
+						break;
+					}
+				}
+				if ( $status === State::error ) {
+					continue;
+				}
 
-                $sended= wp_mail($to, $subject, $message, $headers, $attachments);
+				// Send email
+				global $dcms_mail_real;
+				$dcms_mail_real = true; // real email sending
 
-                $status = $sended ? State::sent : State::error; // 1 ok , -1 error
-                $db->update_email_status($item->id, $status);
+				$sended = wp_mail( $to, $subject, $message, $headers, $attachments );
 
-                usleep(DCMS_ENQUEU_TIME_BETWEEN_MAILS);
-            }
-        }
-    }
+				$status = $sended ? State::sent : State::error; // 1 ok , -1 error
+				$db->update_email_status( $item->id, $status );
+
+				usleep( DCMS_ENQUEU_TIME_BETWEEN_MAILS );
+			}
+		}
+	}
 }
